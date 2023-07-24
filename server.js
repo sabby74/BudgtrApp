6ffe97ae1a23e8eac71a310dd9df8f44f5abb206
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-07-24 18:55:01 -0400

    configured middleware

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const budget = require("./models/budget.js")
+app.set("view engine", "ejs");
+
+//middlewares
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+
+    res.send("hello world");
+  });
 
 
+//index 
 app.get("/budget", (req, res) => {
 
     res.send(budget);
