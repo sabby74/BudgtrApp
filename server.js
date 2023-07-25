4420afe44d160ad6f704ec1aac3e460e045514de
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-07-25 16:11:08 -0400

    new route created

--- server.js
+++ server.js
@@ -1,57 +1,49 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
-const Budget = require("./models/budget.js")
+const Budget = require("./models/budget.js");
 const expressEjsLayouts = require("express-ejs-layouts");
 
 //middlewares
 app.set("view engine", "ejs");
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
+  res.render("index.ejs",{ Budget});
+});
 
-    res.send("hello world");
-  });
-
-
-//index 
+//index
 // app.get("/budget", (req, res) => {
 
 //     res.send(Budget);
 //   });
-  
-app.get("/budget", (req, res) => {
 
-    res.render("index.ejs", {Budget});
-  });
-  
+app.get("/budget", (req, res) => {
+  res.render("index.ejs", { Budget });
+});
 
 //Show
-app.get('/budget/:index', (req, res) => {
-    const { index } = req.params;
-    const budgetItem = Budget[index];
-    res.render('show.ejs', { budgetItem });
-  });
+app.get("/budgets/:index", (req, res) => {
+  const { index } = req.params;
+  const budgetItem = Budget[index];
+  res.render("show.ejs", { budgetItem });
+});
+
+
 //New
-app.get('/budget/new', (req, res) => {
-    
+app.get("/budget/new", (req, res) => {
+  res.render("new.ejs");
+});
 
-    res.render("new.ejs");
-    
-  });
+// app.post('/budget/new', (req, res) => {
+//     console.log(req.body);
+//     const newBudgetItem = req.body;
+//     Budget.push(newBudgetItem);
+//     res.redirect('index.ejs');
+//   });
 
-
-app.post('/budget/new', (req, res) => {
-    console.log(req.body);
-    const newBudgetItem = req.body;
-    Budget.push(newBudgetItem);
-    res.redirect('index.ejs');
-  });
-
-
-
-
-  app.listen(2000 ,() => {
-    console.log("server is listening on port 2000");
+app.listen(2000, () => {
+  console.log("server is listening on port 2000");
 });
