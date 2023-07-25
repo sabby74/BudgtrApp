48d55895fd4ed1c60961805f758afa67e2e7f527
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-07-24 22:03:50 -0400

    added new route

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const Budget = require("./models/budget.js")
 
 //middlewares
 app.set("view engine", "ejs");
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
 
@@ -13,9 +15,14 @@ app.get("/", (req, res) => {
 
 
 //index 
+// app.get("/budget", (req, res) => {
+
+//     res.send(Budget);
+//   });
+  
 app.get("/budget", (req, res) => {
 
-    res.send(Budget);
+    res.render("index.ejs", {Budget});
   });
   
 
